e66f465684ea357b5c8bf5ebdd317f37bb256b93
Siti Nurliyana <32223739+liyanafaireul@users.noreply.github.com>
2017-12-02 01:53:19 +0800

    Create server1.go

---
+++ server1.go
@@ -0,0 +1,26 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "strings" // only needed below for sample processing
+
+func main() {
+
+  fmt.Println("Launching server...")
+
+  // listen on all interfaces
+  ln, _ := net.Listen("tcp", ":8082")
+
+  // accept connection on port
+  conn, _ := ln.Accept()
+
+  // run loop forever (or until ctrl-c)
+  for {
+    // will listen for message to process ending in newline (\n)
+    status, _ := bufio.NewReader(conn).ReadString('\n')
+    // output message received
+    fmt.Print("Marital status:", string(status))
+  }
+}
+//end of code
